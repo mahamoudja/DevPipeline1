2e225355bb61f7a840689669bcd848383551c314
FDevBaig <62343089+FDevBaig@users.noreply.github.com>
2020-07-06 07:32:49 +0100

    Create hello_pipeline1.py
    Master Pipeline

---
+++ hello_pipeline1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from flask import Flask
+
+PORT = 9090
+MESSAGE = """Knowledge necessitates actions, and actions in Islam are only accetped once they fulfil two condtions.<br/><br/>
+these conditions are 1. Doing the action sincerely for Allah.<br/>
+2. That the action is done in accordance to the Sunnah of the Prophet (May the peace and blessings of Allah be upon him).<br/>\n"""
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    result = MESSAGE.encode("utf-8")
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=PORT)
