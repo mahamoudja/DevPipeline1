b7b5bcf2562f1e88319744a520b3ba40e93040ee
FDevBaig <62343089+FDevBaig@users.noreply.github.com>
2020-07-06 21:31:34 +0100

    Update hello_pipeline1.py

--- hello_pipeline1.py
+++ hello_pipeline1.py
@@ -2,7 +2,7 @@
 from flask import Flask
 
 PORT = 9090
-MESSAGE = """!!!!!!!!!!!!!!!!!THE ONE AND ONLY!!!!!!!!!!!!!!!!! Sheikh Mahmood said: <br/>
+MESSAGE = """THE ONE AND ONLY!!!!!!!!!!!!!!!!! Sheikh Mahmood said: <br/>
 Knowledge necessitates actions, and actions in Islam are only accetped once they fulfil two condtions.<br/><br/>
 these conditions are 1. Doing the action sincerely for Allah.<br/>
 2. That the action is done in accordance to the Sunnah of the Prophet (May the peace and blessings of Allah be upon him).<br/>\n"""
