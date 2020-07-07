9ac552e8d6f45fc81e5451ef79293f31767f13d8
FDevBaig <62343089+FDevBaig@users.noreply.github.com>
2020-07-07 14:56:25 +0100

    Update hello_pipeline1.py

--- hello_pipeline1.py
+++ hello_pipeline1.py
@@ -2,7 +2,8 @@
 from flask import Flask
 
 PORT = 9090
-MESSAGE = ''' TESTING!"£ Welcome back Fahad !!!!!!!!!!!!!!!!!Bismillah!!!!!!!!!!!!!!!!!  <br/><br/>
+MESSAGE = ''' AAAAAAAAAAAAAAAAAAAAAAAAAA Welcome back Fahad AAAAAAAAAAAAAAAAAAAAAAAAAAAAA <br/><br/>
+!!!!!!!!!!!!!!!!!Bismillah!!!!!!!!!!!!!!!!!  <br/><br/>
 Knowledge necessitates actions, and actions in Islam are only accetped once they fulfil two condtions.<br/><br/>
 these conditions are 1. Doing the action sincerely for Allah.<br/>
 2. That the action is done in accordance to the Sunnah of the Prophet (May the peace and blessings of Allah be upon him).<br/>\n'''
